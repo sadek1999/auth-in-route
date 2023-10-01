041f7d109ab4a159a9504822a7bf9cd0e7031ce4
Sadek Ali <sadekali1530@gmail.com>
2023-10-01 19:11:46 +0600

    add the code

---
+++ src/fiarebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBggHbKrxyKflQC3FQCmlu7gETDJmnMo-s",
+  authDomain: "auth-in-router.firebaseapp.com",
+  projectId: "auth-in-router",
+  storageBucket: "auth-in-router.appspot.com",
+  messagingSenderId: "951173871478",
+  appId: "1:951173871478:web:905ba2c17a88dda340ec57"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Components/Main.jsx';
+import Home from './Components/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
