41cc95ddd89539e0ad0fceabbcf8297542554bb9
Sadek Ali <sadekali1530@gmail.com>
2023-10-04 22:12:23 +0600

    add the code

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -4,20 +4,24 @@ import { Link } from 'react-router-dom';
 import auth from '../fiarebase/firebase.config';
 
 
-  const handlelogin=e=>{
-    e.preventDefault()
-    const name=e.target.name.value;
-    const email=e.target.email.value;
-    const password =e.target.password.value;
-    console.log(email ,password,name)
-    signInWithEmailAndPassword(auth, email, password)
-    .then(()=>{
-        console.log("successfully log in")
-        e.target.reset()
-    })
-    .catch(error=>console.error(error))
-  }
+  
 const Login = () => {
+        
+    const handlelogin=e=>{
+        e.preventDefault()
+        const name=e.target.name.value;
+        const email=e.target.email.value;
+        const password =e.target.password.value;
+        console.log(email ,password,name)
+        signInWithEmailAndPassword(auth, email, password)
+        .then(()=>{
+            console.log("successfully log in")
+            e.target.reset()
+        })
+        .catch(error=>console.error(error))
+      }
+
+
     return (
         <div className="hero  bg-base-200">
             <div className="hero-content flex-col ">
